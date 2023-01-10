e020a5da2685b4f7a521b78ea495fe459ef1b962
mensimanvar <53275449+mensimanvar@users.noreply.github.com>
2023-01-10 11:48:58 +0530

    Update index.tsx

--- src/state/index.tsx
+++ src/state/index.tsx
@@ -92,7 +92,8 @@ export default function AppStateProvider(props: React.PropsWithChildren<{}>) {
     contextValue = {
       ...contextValue,
       getToken: async (user_identity, room_name) => {
-        const endpoint = process.env.REACT_APP_TOKEN_ENDPOINT || '/token';
+        //const endpoint = process.env.REACT_APP_TOKEN_ENDPOINT || '/token';
+        const endpoint = 'https://twilliodemo.webtechinfoway.com';
 
         return fetch(endpoint, {
           method: 'POST',
