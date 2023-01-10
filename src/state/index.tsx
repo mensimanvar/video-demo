18bcdf0f8377a72206210682a874453c226f5924
nitinmanvar <53275449+mensimanvar@users.noreply.github.com>
2023-01-10 11:45:59 +0530

    token updated

--- src/state/index.tsx
+++ src/state/index.tsx
@@ -93,7 +93,8 @@ export default function AppStateProvider(props: React.PropsWithChildren<{}>) {
       ...contextValue,
       getToken: async (user_identity, room_name) => {
         // const endpoint = process.env.REACT_APP_TOKEN_ENDPOINT || '/token';
-        const endpoint = 'http://localhost:3000/token';
+        //const endpoint = 'http://localhost:3000/token';
+        const endpoint = 'https://twilliodemo.webtechinfoway.com';
 
         return fetch(endpoint, {
           method: 'POST',
